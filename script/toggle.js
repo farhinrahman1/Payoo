ccc3064052ff36e3ec1f2bf4cf9ce90b380665a3
Farhin Rahman <farhin.rahmanp@gmail.com>
2025-10-15 20:26:11 +0600

    Updated Add money and Cash out form

---
+++ script/toggle.js
@@ -0,0 +1,11 @@
+document.getElementById('addMoney').style.display = "block";
+document.getElementById('cashOut').style.display = "none";
+document.getElementById('add-money-box').addEventListener('click', function () {
+    document.getElementById('cashOut').style.display = "none";
+    document.getElementById('addMoney').style.display = "block";
+});
+
+document.getElementById('cash-out-box').addEventListener('click', function () {
+    document.getElementById('addMoney').style.display = "none";
+    document.getElementById('cashOut').style.display = "block";
+});
